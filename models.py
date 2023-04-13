7a72c36b7f086c3a6e3999e5ef8980b7c46c7b53
Allison Ellis <allisonellis@sandiego.edu>
2023-04-12 22:27:51 -0700

    fix databse?

--- models.py
+++ models.py
@@ -12,10 +12,10 @@ class ChatData(Base):
     responses = Column(Text)
     analysis = Column(Text)
 
-# Get the database URL from the environment variable
-database_url = os.environ['postgres://fobglwnekkiazp:d39c346a5fd7173d11de2f75081b0cf04de82e443cb96102f890d6d4ce93cbcb@ec2-3-92-151-217.compute-1.amazonaws.com:5432/dbdlppji0fu42o']
+# Set the DATABASE_URL variable to the connection string
+DATABASE_URL = os.environ.get('DATABASE_URL')
 
-engine = create_engine(database_url)
+engine = create_engine(DATABASE_URL)
 Base.metadata.create_all(engine)
 
 Session = sessionmaker(bind=engine)
